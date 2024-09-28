a9ad7a142cf6f797c92c0a30f77c732323cbe44b
Your Name <you@example.com>
2024-09-28 10:28:09 -0500

    JDT: Updated requirements.txt.

--- exhibit_maker.py
+++ exhibit_maker.py
@@ -22,6 +22,7 @@ Author: James D. Triveri
 """
 import argparse
 import configparser
+import os
 from pathlib import Path
 
 import matplotlib as mpl
@@ -68,26 +69,11 @@ def main():
     
     # Read from settings.cfg.
     config = configparser.ConfigParser()
-    config.read("settings.cfg")
-
-    # course_desc = config["global"].get(course_desc).strip()
-    # cfg_csv_path = config["global"].get(csv_path).strip()
-    # cfg_img_path = config["global"].get(img_path).strip()
-    # cfg_module = config["global"].getint(module)
-    # cfg_cmap = config["global"].get("cmap").strip()
-    # verbose = config["global"].getboolean("verbose")
-
+    cfg_path = os.path.join(os.path.dirname(__file__), "settings.cfg")
+    config.read(cfg_path)
 
     # Parse command line arguments. 
     argvs = parser.parse_args()
-
-    # argvs_csv_path = argvs.csv_path.strip()
-    # argvs_img_path = argvs.img_path.strip()
-    # argvs_module = argvs.module
-    # argvs_cmap = argvs.cmap.strip()
-    # argvs_course_desc = argvs.course_desc.strip()
-
-
 
     if (course_desc := argvs.course_desc) is None:
         course_desc = config["global"].get("course_desc").strip()
@@ -206,9 +192,11 @@ def main():
         max_score = df[df.assignment==assign].dropna(how="any")["grade"].max()
         gg = gg.assign(grade=gg["grade"].astype(str), n=gg["n"].astype(int))
         gg.plot.bar(ax=ax[ii], color=color)
-        
+
         ax[ii].set_title(f"{desc}", fontsize=facet_title_fontsize, weight="normal")
         ax[ii].set_xticklabels(gg["grade"].values, rotation=0)
+        ax[ii].set_yticks(np.arange(3, nbr_students, 3 ,dtype=int))
+
         ax[ii].set_xlabel("score", fontsize=axis_label_fontsize)
         ax[ii].set_ylabel("nbr. students", fontsize=axis_label_fontsize)
         ax[ii].tick_params(axis="x", which="major", direction='in', labelsize=tick_label_fontsize)
